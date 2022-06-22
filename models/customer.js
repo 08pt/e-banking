616dfb75cb1178dc8d52e92ef0c94ab352c7464a
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2022-06-22 07:05:02 +0530

    commit

---
+++ models/customer.js
@@ -0,0 +1,29 @@
+//The models folder, will contain the files 
+//which will define the MongoDB schema
+
+
+
+// models/customer.js
+const mongoose = require("mongoose");
+
+const CustomerSchema = new mongoose.Schema({
+    customer_id: {
+        type: Number,
+        required: true,
+    },
+    customer_name: {
+        type:String,
+    },
+    customer_address: {
+        type:String,
+    },
+    customer_phone_no: {
+        type:Number,
+    },
+
+    
+});
+
+const Customer = mongoose.model("todo", CustomerSchema);
+
+module.exports = Customer;
