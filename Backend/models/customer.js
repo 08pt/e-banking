21cd6c0d0fa45f4aa1eec770a38dd04cfc1a8bc7
SwathiReddy0503 <103940224+SwathiReddy0503@users.noreply.github.com>
2022-06-23 10:24:52 +0530

    Update customer.js

--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -1,35 +1,11 @@
-//The models folder, will contain the files 
-//which will define the MongoDB schema
-
-
-
-// models/customer.js
-const mongoose = require("mongoose");
-
-const CustomerSchema = new mongoose.Schema({
-    customer_id: {
-        type: Number,
-        required: true,
-    },
-    customer_name: {
-        type:String,
-    },
-    
-    customer_email: {
-        type:String,
-    },
-    password: {
-        type:Number,
-    },
-    customer_mobile: {
-        type:Number,
-    },
-    customer_address: {
-        type:String,
-    },
-    
-});
-
-const Customer = mongoose.model("customer", CustomerSchema);
-
-module.exports = Customer;
+const mongoose=require('mongoose')
+ const Customer=mongoose.model('Customer',{
+     cust_id:{type:Number},
+     cust_name:{type:String},
+     email:{type:String},
+     password:{type:Number},
+     phone_no:{type:Number},
+     address : {type:String}
+       
+      });
+ module.exports={Customer};
