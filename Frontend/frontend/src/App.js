2eebaa7ead6234d19efee5f4753c00314e25a115
psindhuja98 <65760969+psindhuja98@users.noreply.github.com>
2022-06-23 23:13:44 +0530

    Update App.js

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,22 +1,23 @@
 
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Main from './components/main';
+//import logo from './logo.svg';
+import './App.css';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import ShowAccount from './components/ShowAccount';
+import Router from './components/Router';
+import AddAccount1 from './components/AddAccount1' ;
 
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-      <div className="container">
-      
-        <Main/>
-      </div>
-    </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Router/>
+     <Routes>
+     <Route path="/" element={<AddAccount1/>}/>
+     <Route path="/showAccount" element={<ShowAccount/>}/>
+     <Route path="/addAccount" element={<AddAccount1/>}/>
+     
+     </Routes>
+    </div>
+  );
 }
 
 export default App;
